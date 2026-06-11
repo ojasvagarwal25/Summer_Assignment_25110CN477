86142c6be9b35c8ee1f0f671211e560b228c8810
ojasvagarwal25 <ojasvagarwal25@gmail.com>
2026-06-11 18:47:30 +0530

    Add files via upload

---
+++ Day-11/ques43.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// Function to check prime number
+int isPrime(int n) {
+
+    if(n <= 1)
+        return 0;
+
+    for(int i = 2; i * i <= n; i++) {
+
+        if(n % i == 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(isPrime(n))
+        printf("Prime Number\n");
+    else
+        printf("Not a Prime Number\n");
+
+    return 0;
+}
